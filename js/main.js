cede28123cee1e1edd01b86717faf624d236db72
GMOS <gabriela.m.ordonezs@gmail.com>
2025-04-11 17:32:35 -0600

    Se agregan la funcion getCategories para poder ver los productor por categorias

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const main = document.getElementsByTagName("main").item(0);
 const URLmain = "https://fakestoreapi.com/products/";
+const ulMenu = document.getElementById("ulMenu");
 const cardsContainer = document.getElementById("cards-container");
 
-function getData(){
-    fetch(URLmain)
+function getData(cat){
+  const options ={"method": "GET"};
+    fetch(URLmain+cat, options)
     .then((response)=>{
         console.log(response);
         response.json().then((res)=>{
@@ -22,22 +24,46 @@ function getData(){
 
 }//getData
 
-getData();
+function getCategories(){
+  const options ={"method": "GET"};
+  fetch(URLmain+"categories/", options)
+  .then((response) => {
+      response.json().then((res)=>{
+          // console.log("categories:", res);
+          res.forEach((cat)=>{
+            ulMenu.insertAdjacentHTML("afterbegin",
+              `<li><a class="dropdown-item" onclick="getData('category/${cat.replace("'","%27")}');">${cat}</a></li>`);
+          });
+      });
+  })
+  .catch((err)=>{
+      main.insertAdjacentHTML("beforeend",
+          `<div class="alert alert-danger" role="alert">
+              ${err.message}
+          </div>`
+      );
+  });
+
+}//getCategories
+
+getCategories();
+getData("");
 
 function createCards(prods) {
-    let cardsHTML = '';
-    prods.forEach(prod => {
-      const cardHTML = `
-        <div id="${prod.id}" class="card p-2" style="width: 18rem;">
-          <div class="ratio ratio-1x1">
-            <img src="${prod.image}" class="card-img-top" alt="${prod.title}" style="object-fit: contain;">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">${prod.title}</h5>
-            <p class="card-text text-truncate">${prod.description}</p>
-            <a href="#" class="btn btn-primary">Ver más</a>
-          </div>
-        </div>`;
-      cardsContainer.insertAdjacentHTML("beforeend", cardHTML);
-    });
+  cardsContainer.innerHTML = '';
+  let cardsHTML = '';
+  prods.forEach(prod => {
+    const cardHTML = `
+      <div id="${prod.id}" class="card p-2" style="width: 18rem;">
+        <div class="ratio ratio-1x1">
+          <img src="${prod.image}" class="card-img-top" alt="${prod.title}" style="object-fit: contain;">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${prod.title}</h5>
+          <p class="card-text text-truncate">${prod.description}</p>
+          <a href="#" class="btn btn-primary">Ver más</a>
+        </div>
+      </div>`;
+    cardsContainer.insertAdjacentHTML("beforeend", cardHTML);
+  });
 }//createCards
